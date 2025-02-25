81d6d2184e2560e478b1ee5d32d4e18db5f26e5a
Tom de Visser <tomdevisser29@gmail.com>
2025-02-25 12:06:04 +0100

    Enhance FlexibleContentUtils with custom layout actions and improved paste functionality

--- js/acf-flexible-content-utils.js
+++ js/acf-flexible-content-utils.js
@@ -15,6 +15,9 @@ class FlexibleContentUtils {
     });
   }
 
+  /**
+   * These global actions are added to the postbox header of the layout group.
+   */
   addCustomLayoutActions() {
     const postboxHeader = document.querySelector(
       ".acf-postbox .handle-actions"
@@ -36,6 +39,9 @@ class FlexibleContentUtils {
     }
   }
 
+  /**
+   * These custom controls are added to each layout in the flexible content field.
+   */
   addCustomLayoutControls() {
     acf.addAction("ready_field/type=flexible_content", (field) => {
       this.layouts[field.data.key] = field;
@@ -57,6 +63,9 @@ class FlexibleContentUtils {
     });
   }
 
+  /**
+   * Copies the layout to the clipboard.
+   */
   copyLayout(e) {
     const target = e.target;
     const layoutEl = target.closest(".layout");
@@ -88,18 +97,15 @@ class FlexibleContentUtils {
     }
   }
 
+  /**
+   * Pastes the copied layout to the current flexible content field.
+   */
   pasteLayout() {
     const data = prompt("Paste in the copied layout you'd like to insert.");
     if (!data) return;
 
     try {
       const { domain, acfFieldKey, layout } = JSON.parse(data);
-
-      // const layoutHtml = layout.closest("[data-layout]");
-      const container = document.createElement("div");
-      container.innerHTML = layout;
-
-      const layoutsHtml = container.querySelectorAll("[data-layout]");
 
       const layoutGroupKey = document.querySelector(
         ".acf-field-flexible-content"
@@ -110,25 +116,117 @@ class FlexibleContentUtils {
       flexibleContentModel.cid = this.layouts[layoutGroupKey].cid;
       this.currentFlexibleContent = flexibleContentModel;
 
+      const container = document.createElement("div");
+      container.innerHTML = layout;
+
+      const pastesLayoutsHtml = container.querySelectorAll("[data-layout]");
+      if (!pastesLayoutsHtml.length) {
+        alert("No layouts found in the pasted data.");
+        return;
+      }
+
+      const acfLayoutsList = flexibleContentModel.$popup().html();
+      const currentLayouts = flexibleContentModel.$layouts();
+
+      const countLayouts = (name) => {
+        return currentLayouts.filter(
+          (layout) => currentLayouts[layout].dataset.layout === name
+        ).length;
+      };
+
       const validatedLayouts = [];
 
-      layoutsHtml.forEach((layoutHtml) => {
+      pastesLayoutsHtml.forEach((layoutHtml) => {
         const layoutName = layoutHtml.dataset.layout;
+        const layoutCount = countLayouts(layoutName);
+
+        const getLayoutClone = flexibleContentModel.$clone(layoutName);
+        if (!getLayoutClone) {
+          alert(`Layout ${layoutName} is not allowed in this group.`);
+          return;
+        }
+
         validatedLayouts.push(layoutHtml);
       });
 
+      if (!validatedLayouts.length) {
+        alert("No valid layouts found in the pasted data.");
+        return;
+      }
+
       validatedLayouts.forEach((layoutHtml) => {
-        const dataId = layoutHtml.dataset.id;
+        const layoutId = layoutHtml.dataset.id;
+        const fieldIdentifier = `${acfFieldKey}[${layoutId}]`;
         const targetGroup = flexibleContentModel
           .$control()
-          .find("input[type=hidden]")
+          .find("> input[type=hidden]")
           .attr("name");
 
-        const uniqId = acf.uniqid();
+        this.insertLayout({
+          layoutHtml,
+          fieldIdentifier,
+          targetGroup,
+        });
       });
     } catch (e) {
       console.error("Error while pasting layout: ", e);
     }
+  }
+
+  /**
+   * Inserts the layout into the flexible content field.
+   */
+  insertLayout({ layoutHtml, fieldIdentifier, targetGroup }) {
+    const flexibleContentModel = this.currentFlexibleContent;
+    if (!flexibleContentModel.allowAdd()) {
+      alert("You cannot add any more layouts.");
+      return;
+    }
+
+    const uniqid = acf.uniqid();
+    let newIdentifier = "";
+
+    if (targetGroup) {
+      if (!fieldIdentifier) {
+        fieldIdentifier = `${targetGroup}[${layoutHtml.dataset.id}]`;
+      }
+      newIdentifier = `${targetGroup}[${uniqid}]`;
+    }
+
+    acf.doAction("before_duplicate", layoutHtml);
+
+    const duplicatedLayout = layoutHtml.cloneNode(true);
+
+    acf.rename({
+      target: jQuery(duplicatedLayout),
+      search: fieldIdentifier,
+      replace: newIdentifier,
+    });
+
+    acf.doAction(
+      "after_duplicate",
+      jQuery(layoutHtml),
+      jQuery(duplicatedLayout)
+    );
+
+    const appendLayout = flexibleContentModel.proxy((duplicatedLayout) => {
+      duplicatedLayout = jQuery(duplicatedLayout);
+
+      duplicatedLayout.addClass("strl-appended-layout");
+      duplicatedLayout.attr("data-id", uniqid);
+
+      flexibleContentModel.$layoutsWrap().append(duplicatedLayout);
+
+      acf.enable(duplicatedLayout, flexibleContentModel.cid);
+      flexibleContentModel.render();
+    });
+
+    appendLayout(duplicatedLayout);
+
+    acf.doAction("append", jQuery(duplicatedLayout));
+
+    // Trigger a change event for validation purposes.
+    flexibleContentModel.$input().trigger("change");
   }
 }
 
